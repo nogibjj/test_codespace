5c76a4a92a4612e99e5ba189dbca05a552ca4f98
Fangting Ma <64255604+Erica233@users.noreply.github.com>
2022-09-17 19:50:43 +0000

    container

--- hello.py
+++ hello.py
@@ -1,23 +1,26 @@
 import random
 
-#def hello():
+# def hello():
 #    print("hello, world!")
 
-#hello()
+# hello()
 
-#write a decorator function logs input and output and times it to a function
+# write a decorator function logs input and output and times it to a function
 def log(func):
     def wrapper(*args, **kwargs):
         print("Calling {} with arguments {}".format(func.__name__, args))
         result = func(*args, **kwargs)
         print("{} returned {}".format(func.__name__, result))
         return result
+
     return wrapper
 
-#invoke decorator function using hello function
+
+# invoke decorator function using hello function
 @log
 def hello(x):
     print(f"Hello, World! {x}")
+
 
 hello(3)
 
@@ -28,6 +31,6 @@ def fruit_generator():
         yield fruits[random.randint(0, 2)]
 
 
- #invoke fruit_generator function and print the result of the generator for five runs
+# invoke fruit_generator function and print the result of the generator for five runs
 for i in range(5):
     print(next(fruit_generator()))
