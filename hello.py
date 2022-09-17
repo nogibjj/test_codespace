b0896b0cc102f7e820303fcf30d751a15ae5667b
Fangting Ma <64255604+Erica233@users.noreply.github.com>
2022-09-17 06:38:05 +0000

    1

--- hello.py
+++ hello.py
@@ -2,3 +2,19 @@ def hello():
     print("hello, world!")
 
 hello()
+
+#write a decorator function logs input and output and times it to a function
+def log(func):
+    def wrapper(*args, **kwargs):
+        print("Calling {} with arguments {}".format(func.__name__, args))
+        result = func(*args, **kwargs)
+        print("{} returned {}".format(func.__name__, result))
+        return result
+    return wrapper
+
+#invoke decorator function using hello function
+@log
+def hello():
+    print("Hello, World!")
+
+hello()
